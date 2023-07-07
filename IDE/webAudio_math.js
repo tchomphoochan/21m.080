fcbc630279545d212591fba3ced94458b100a1ba
ianhattwick <ian@ianhattwick.com>
2023-07-07 15:56:36 -0400

    initial live coding interface

---
+++ IDE/webAudio_math.js
@@ -0,0 +1,35 @@
+//math
+floor = function(val){
+  if(val < -100000) return _
+  return Math.floor(val)
+}
+
+ceil = function(val){
+  if(val < -100000) return _
+  return Math.ceil(val)
+}
+
+peak = function(val){
+  if(val < -100000) return _
+  return Math.ceil(val)
+}
+
+round = function(val){
+  if(val < -100000) return _
+  return Math.round(val)
+}
+
+trunc = function(val){
+  if(val < -100000) return _
+  return Math.trunc(val)
+}
+
+abs = function(val){
+  if(val < -100000) return _
+  return Math.abs(val)
+}
+
+cos = function(val){
+  if(val < -100000) return _
+  return Math.cos(val)
+}
