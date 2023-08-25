e10fc665323237eafa31232bde0eac015c5cebe0
ianhattwick <ian@ianhattwick.com>
2023-08-25 13:16:25 -0400

    Changed import of midiCoder. . .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-import midiMain from './midiCoder/midi_main';
+//mport midiMain from './midiCoder/midi_main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
