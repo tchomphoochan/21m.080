dbf5fb402b1f80009eb7c56fcf7459334961091a
ianhattwick <ian@ianhattwick.com>
2023-09-11 14:14:09 -0400

    Added audio samples

--- src/pages/Template.js
+++ src/pages/Template.js
@@ -1,6 +1,6 @@
 // import { useState, useEffect } from "react";
 import Editor from '../Editor.js';
-import './Template.css';
+// import './Template.css';
 
 const Template = (props) => {
     return (
